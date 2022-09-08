26261e095f588a26aa3d3d16a803fa9ac3012adc
Newaz579 <newazsharif329@gmail.com>
2022-09-09 01:13:58 +0600

    third push

--- js/meals.js
+++ js/meals.js
@@ -11,11 +11,11 @@ const displayLoadMeals = (meals) =>{
     const mealsContainer = document.getElementById('meals-container');
     mealsContainer.innerHTML = ``;
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal);
         const mealDiv = document.createElement('div');
-        mealDiv.classList.add('col');
+        mealDiv.classList.add('card');
         mealDiv.innerHTML = `
-            <div onclick="displayMealDetail(${meal.idMeal})" class="card">
+            <div onclick="loadMealDetail(${meal.idMeal})" class="card">
                 <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -33,7 +33,26 @@ const searchFood = () =>{
     loadMeals(searchValue);
 }
 
-const displayMealDetail = (mealID) =>{
-    const url = `www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`;
-    console.log(url);
+const loadMealDetail = (mealID) =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`;
+    // console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayMealDetail(data.meals[0]))
+}
+
+const displayMealDetail = (meal)=> {
+    const mealContainer = document.getElementById('meal-container');
+    mealContainer.innerHTML = ``;
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(1,100)}.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    `;
+    mealContainer.appendChild(div);
 }
